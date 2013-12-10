1dd5f7f51d4d7cbdbd3b499b084a9a992ae8037f
Parker Moore <parkrmoore@gmail.com>
2013-12-10 22:48:53 +0000

    Kill old ruby-install installs in bootstrap script

--- boots/ruby-install.sh
+++ boots/ruby-install.sh
@@ -4,6 +4,12 @@ $RB_BUILD_GIT_URL="https://github.com/sstephenson/ruby-build.git"
 $RB_BUILD_INSTALL_DIR="~/.rbenv/plugins/ruby-build"
 $RB_BUILD_VERSION="v20131206"
 
+if [ -d "$RB_BUILD_INSTALL_DIR" ]; then
+  rm -rf $RB_BUILD_INSTALL_DIR
+fi
+
+mkdir -p $RB_BUILD_INSTALL_DIR
+
 git clone $RB_BUILD_GIT_URL $RB_BUILD_INSTALL_DIR
 cd $RB_BUILD_INSTALL_DIR
 git checkout $RB_BUILD_VERSION
