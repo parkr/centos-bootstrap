74b8f7fbe51540c70c1245e2cad30b1de281e314
Parker Moore <parkrmoore@gmail.com>
2013-12-10 22:57:46 +0000

    Check out a particular version of rbenv

--- boots/rbenv.sh
+++ boots/rbenv.sh
@@ -2,13 +2,25 @@
 
 RBENV_GIT_URL="https://github.com/sstephenson/rbenv.git"
 RBENV_INSTALL_DIR="$HOME/.rbenv"
+RBENV_VERSION="v0.4.0"
 
-if [ -d "$HOME/.rbenv" ]; then
-  rm -rf "$HOME/.rbenv"
+if [ -z "$(which git)" ]; then
+  echo "Install git first: ./bootstrap git"
+  exit 1
 fi
 
-git clone $RBENV_GIT_URL "$HOME/.rbenv"
+if [ -d "$RBENV_INSTALL_DIR" ]; then
+  rm -rf "$RBENV_INSTALL_DIR"
+fi
+
+git clone $RBENV_GIT_URL "$RBENV_INSTALL_DIR"
+cd $RBENV_INSTALL_DIR
+git checkout $RBENV_VERSION
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+
 exec $SHELL -l
 type rbenv
+
+echo "It's recommended that you install ruby-install now. :)"
