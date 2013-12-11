c93675c48a6daa63a4e9ce3bf549fc0e7346cd09
Parker Moore <parkrmoore@gmail.com>
2013-12-11 02:42:57 +0000

    Run 'make install-doc' run as USER not as ROOT

--- boots/git.sh
+++ boots/git.sh
@@ -10,4 +10,5 @@ wget https://github.com/git/git/archive/v${VERSION}.zip
 unzip v${VERSION}
 
 cd git-${VERSION}
-sudo make prefix=/usr install install-doc install-html install-info
+sudo make prefix=/usr install install-html install-info
+make install-doc
