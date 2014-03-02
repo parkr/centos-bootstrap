1261274126a7391715ba716a6c720697d8744885
Parker Moore <parkrmoore@gmail.com>
2014-03-02 20:15:11 +0000

    Update to camlistore 0.6

--- boots/camlistore.sh
+++ boots/camlistore.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-VERSION="0.5"
+VERSION="0.6"
 FILENAME="camlistore-${VERSION}"
 GZ_FILENAME="${FILENAME}.tar.gz"
 
-curl "https://codeload.github.com/bradfitz/camlistore/tar.gz/0.5" -o $GZ_FILENAME
+curl "https://codeload.github.com/bradfitz/camlistore/tar.gz/${VERSION}" -o $GZ_FILENAME
 tar -xzvf $GZ_FILENAME
 
 cd $FILENAME
 go run make.go
 sudo cp bin/* /usr/bin/ -v
+
 cd -
+rm $GZ_FILENAME
+rm -r $FILENAME
