9b6a4627f78e10dc9d4e1d2f658e4d1c3e0bbf28
Parker Moore <parkrmoore@gmail.com>
2014-03-04 02:46:35 +0000

    Fixed autoconf and ruby boots.

--- boots/autoconf.sh
+++ boots/autoconf.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/env bash
 
+set -ex
+
 VERSION=2.69
 
 cd /usr/src
 sudo wget http://ftp.gnu.org/gnu/autoconf/autoconf-${VERSION}.tar.gz
 sudo tar -xzvf autoconf-${VERSION}.tar.gz
 cd autoconf-${VERSION}
-sudo ./configure PREFIX=/usr
+sudo ./configure --prefix=/usr
 sudo make
 sudo make install
 sudo make installcheck
--- boots/ruby.sh
+++ boots/ruby.sh
@@ -1,17 +1,24 @@
 #! /usr/bin/env bash
-
 set -xe
 
 # ruby 2.1.0
 RUBY_VERSION=2.1.1
+FOLDER_NAME="ruby-${RUBY_VERSION}"
+TAR_FILE="${FOLDER_NAME}.tar.gz"
 cd /usr/src
-sudo wget ftp://ftp.ruby-lang.org/pub/ruby/2.1/ruby-$RUBY_VERSION.tar.gz 
-sudo tar -zxvf ruby-$RUBY_VERSION.tar.gz
-cd ruby-$RUBY_VERSION
+
+if [ ! -d "${FOLDER_NAME}" ]; then
+  if [ ! -f "${TAR_FILE}" ]; then
+    sudo wget ftp://ftp.ruby-lang.org/pub/ruby/2.1/${TAR_FILE}
+  fi
+  sudo tar -zxvf ${TAR_FILE}
+fi
+
+cd ${FOLDER_NAME}
 sudo autoconf
-sudo ./configure PREFIX=/usr
+sudo ./configure --prefix=/usr
 sudo make
 sudo make install
 /usr/bin/ruby --version
- 
-./bootstrap rubygems
+
+echo "Great! Ruby $(/usr/bin/ruby --version) is all installed. Now bootstrap RubyGems to finish up."
