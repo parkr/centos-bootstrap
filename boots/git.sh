66c5416f99fc49a9ddfcd4136db5856f405befb5
Parker Moore <parkrmoore@gmail.com>
2016-06-30 01:41:11 +0000

    Upgrade git to v2.9.0

--- boots/git.sh
+++ boots/git.sh
@@ -2,11 +2,22 @@
 
 set -e
 
-VERSION="2.1.4"
+sudo yum install docbook2X
+sudo ln -s /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
 
-wget https://github.com/git/git/archive/v${VERSION}.zip
-unzip v${VERSION}
+VERSION="2.9.0"
+ZIP_FILE="/tmp/v${VERSION}.zip"
+WORK_DIR="/tmp/git-${VERSION}"
 
-cd git-${VERSION}
-sudo make prefix=/usr install install-html install-info
-make install-doc
+[ -f $ZIP_FILE ] || wget https://github.com/git/git/archive/v${VERSION}.zip -O $ZIP_FILE
+[ -d $WORK_DIR ] || unzip $ZIP_FILE -d $WORK_DIR
+
+pushd "$WORK_DIR/git-${VERSION}"
+make configure
+./configure --prefix=/usr
+make all doc info
+sudo make install
+sudo make -e DOCBOOK2X_TEXI=$(which docbook2texi) install-doc
+popd
+
+rm -r $WORK_DIR $ZIP_FILE
